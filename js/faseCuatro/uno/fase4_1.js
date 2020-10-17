65ccbe8eb5f532edd5f2d4d5904b1469b7a2281a
Efrain Vergara <efrainvergara.udec@gmail.com>
2020-10-17 15:16:21 -0500

    Add four Stage

---
+++ js/faseCuatro/uno/fase4_1.js
@@ -0,0 +1,76 @@
+function irPreguntaCuatro_Dos() {
+    document.getElementById('fase4_uno').style.display = 'none';
+    document.getElementById('fase4_dos').style.display = '';
+    document.getElementById('fase4_tres').style.display = 'none';
+    document.getElementById('fase4_cuatro').style.display = 'none';
+    document.getElementById('fase4_cinco').style.display = 'none';
+    document.getElementById('fase4_seis').style.display = 'none';
+    document.getElementById('fase4_siete').style.display = 'none';
+    document.getElementById('fase4_ocho').style.display = 'none';
+}
+
+function irPreguntaCuatro_Tres() {
+    document.getElementById('fase4_uno').style.display = 'none';
+    document.getElementById('fase4_dos').style.display = 'none';
+    document.getElementById('fase4_tres').style.display = '';
+    document.getElementById('fase4_cuatro').style.display = 'none';
+    document.getElementById('fase4_cinco').style.display = 'none';
+    document.getElementById('fase4_seis').style.display = 'none';
+    document.getElementById('fase4_siete').style.display = 'none';
+    document.getElementById('fase4_ocho').style.display = 'none';
+}
+
+function irPreguntaCuatro_Cuatro() {
+    document.getElementById('fase4_uno').style.display = 'none';
+    document.getElementById('fase4_dos').style.display = 'none';
+    document.getElementById('fase4_tres').style.display = 'none';
+    document.getElementById('fase4_cuatro').style.display = '';
+    document.getElementById('fase4_cinco').style.display = 'none';
+    document.getElementById('fase4_seis').style.display = 'none';
+    document.getElementById('fase4_siete').style.display = 'none';
+    document.getElementById('fase4_ocho').style.display = 'none';
+}
+
+function irPreguntaCuatro_Cinco() {
+    document.getElementById('fase4_uno').style.display = 'none';
+    document.getElementById('fase4_dos').style.display = 'none';
+    document.getElementById('fase4_tres').style.display = 'none';
+    document.getElementById('fase4_cuatro').style.display = 'none';
+    document.getElementById('fase4_cinco').style.display = '';
+    document.getElementById('fase4_seis').style.display = 'none';
+    document.getElementById('fase4_siete').style.display = 'none';
+    document.getElementById('fase4_ocho').style.display = 'none';
+}
+
+function irPreguntaCuatro_Seis() {
+    document.getElementById('fase4_uno').style.display = 'none';
+    document.getElementById('fase4_dos').style.display = 'none';
+    document.getElementById('fase4_tres').style.display = 'none';
+    document.getElementById('fase4_cuatro').style.display = 'none';
+    document.getElementById('fase4_cinco').style.display = 'none';
+    document.getElementById('fase4_seis').style.display = '';
+    document.getElementById('fase4_siete').style.display = 'none';
+    document.getElementById('fase4_ocho').style.display = 'none';
+}
+
+function irPreguntaCuatro_Siete() {
+    document.getElementById('fase4_uno').style.display = 'none';
+    document.getElementById('fase4_dos').style.display = 'none';
+    document.getElementById('fase4_tres').style.display = 'none';
+    document.getElementById('fase4_cuatro').style.display = 'none';
+    document.getElementById('fase4_cinco').style.display = 'none';
+    document.getElementById('fase4_seis').style.display = 'none';
+    document.getElementById('fase4_siete').style.display = '';
+    document.getElementById('fase4_ocho').style.display = 'none';
+}
+
+function irPreguntaCuatro_Ocho() {
+    document.getElementById('fase4_uno').style.display = 'none';
+    document.getElementById('fase4_dos').style.display = 'none';
+    document.getElementById('fase4_tres').style.display = 'none';
+    document.getElementById('fase4_cuatro').style.display = 'none';
+    document.getElementById('fase4_cinco').style.display = 'none';
+    document.getElementById('fase4_seis').style.display = 'none';
+    document.getElementById('fase4_siete').style.display = 'none';
+    document.getElementById('fase4_ocho').style.display = '';
+}
---
+++ js/faseCuatro/uno/logic.js
@@ -0,0 +1,9 @@
+//Fase 4_1
+document.getElementById('fase4_uno').style.display = '';
+document.getElementById('fase4_dos').style.display = 'none';
+document.getElementById('fase4_tres').style.display = 'none';
+document.getElementById('fase4_cuatro').style.display = 'none';
+document.getElementById('fase4_cinco').style.display = 'none';
+document.getElementById('fase4_seis').style.display = 'none';
+document.getElementById('fase4_siete').style.display = 'none';
+document.getElementById('fase4_ocho').style.display = 'none';
